2c4e4da9aab3324ee99a8f07d741b42b72bfb2f5
Parker Murray <parker@pmurray.io>
2025-02-28 14:41:54 -0800

    Inlined everything in the main function to make translation to other language easier, split file I/O to the after the simulation run

--- C++/main.cpp
+++ C++/main.cpp
@@ -53,52 +53,77 @@ void PPMImage::exportImage(std::string path)
 
 }
 
-void initGrid(int *grid, int len)
-{
-	for(int i = 0; i < len; i++)
-	{
-		grid[i] = rand() % 2;
-	}
-}
-
-void updateCell(int *grid, int *grid_buf, int i, int j, int rows, int cols)
-{
-	int neighborSum = 0;
-	int idx = i * cols + j;
-	int cellValue = grid[idx];
-
-	neighborSum = grid[((i - 1) % rows) * cols + ((j - 1) % cols)] + grid[i * cols + ((j - 1) % cols)] + grid[((i + 1) % rows) * cols + ((j - 1) % cols)] +
-	grid[((i - 1) % rows) * cols + ((j + 1) % cols)] + grid[i * cols + ((j + 1) % cols)] + grid[((i + 1) % rows) * cols + ((j + 1) % cols)] +
-	grid[((i - 1) % rows) * cols + j] + grid[((i + 1) % rows) * cols + j];
-
-	grid_buf[idx] = ((cellValue == 0) * (neighborSum == 3)) + ((cellValue == 1) * ((neighborSum == 2) + (neighborSum == 3)));
-}
-
 int main(int argc, char **argv)
 {
 	const int rows = 100;
 	const int cols = 100;
-	
-	int *grid = (int *) calloc(rows * cols, sizeof(int));
-	int *grid_buf = (int *) calloc(rows * cols, sizeof(int));
+	const int outImgScale = 10;
+	const int _width = cols * outImgScale;
+	const int _height = rows * outImgScale;
+	const int numIter = 10;
+	std::vector<int*> states;
 
-	initGrid(grid, rows * cols);
-	
-	for(int i = 0; i < rows; i++)
+	for(int i = 0; i < numIter + 1; i++)
 	{
-		for(int j = 0; j < cols; j++)
+		int *grid = (int *) calloc(rows * cols, sizeof(int));
+		states.push_back(grid);
+	}
+
+	//Initializing state to alternating 0 and 1;
+	for(int i = 0; i < rows * cols; i++)
+	{
+		states[0][i] = rand() % 2;
+	}
+
+	for(int iter = 1; iter < numIter + 1; iter++)
+	{
+		int* grid = states[iter - 1];
+		int* grid_buf = states[iter];
+		for(int i = 0; i < rows; i++)
 		{
-			updateCell(grid, grid_buf, i, j, rows, cols);
+			for(int j = 0; j < cols; j++)
+			{
+				int neighborSum = 0;
+				int idx = i * cols + j;
+				int cellValue = grid[idx];
+
+				neighborSum = grid[((i - 1) % rows) * cols + ((j - 1) % cols)] + grid[i * cols + ((j - 1) % cols)] + grid[((i + 1) % rows) * cols + ((j - 1) % cols)] +
+				grid[((i - 1) % rows) * cols + ((j + 1) % cols)] + grid[i * cols + ((j + 1) % cols)] + grid[((i + 1) % rows) * cols + ((j + 1) % cols)] +
+				grid[((i - 1) % rows) * cols + j] + grid[((i + 1) % rows) * cols + j];
+
+				grid_buf[idx] = ((cellValue == 0) * (neighborSum == 3)) + ((cellValue == 1) * ((neighborSum == 2) + (neighborSum == 3)));
+			}
 		}
 	}
 
-	memcpy(grid, grid_buf, sizeof(int) * rows * cols);
+	for(int iter = 0; iter < states.size(); iter++)
+	{
+		std::string path = "out/" + std::to_string(iter) + ".ppm";
+		std::ofstream outFile;
+		outFile.open(path);
 
-	PPMImage outImage = PPMImage(cols, rows, grid);
-	outImage.exportImage("out.ppm");
-	 	
-	free(grid);
-	free(grid_buf);
+		if(!outFile.is_open())
+		{
+			std::cout << "File " << path << " could not be opened" << std::endl;
+			return 1;
+		}
+
+		outFile << "P1" << std::endl;
+		outFile << _height << " " << _width << std::endl;
+		for (int i = 0; i < _height; i++)
+		{
+			for (int j = 0; j < _width; j++)
+			{
+				int i_scale = floor(i / outImgScale);
+				int j_scale = floor(j / outImgScale);
+				int idx = i_scale * (_width / outImgScale) + j_scale;
+				outFile << states[iter][idx];
+			}
+			outFile << std::endl;
+		}
+		outFile.close();
+	}
+
 	return 0;
 }
 
